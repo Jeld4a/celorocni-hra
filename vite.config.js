6acd066eeddd27be93fa8ecb54b5eb70534a3648
Jeld4a <38851542+Jeld4a@users.noreply.github.com>
2025-09-24 17:05:16 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/celorocni-hra/',
   plugins: [react()],
-  base: '/celorocni-hra/' // důležité pro GitHub Pages
-})
+});
